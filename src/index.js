3099f1864a44199fc15cfd2bccbcd4277bca6992
yunj <yunj8649@xingxingmobility.com>
2021-03-27 14:13:06 +0900

    chapter9_commit before npm eject

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './five/App';
 // import App from './six/App';
-import App from './eight/App';
+// import App from './eight/App';
+import App from './nine/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
